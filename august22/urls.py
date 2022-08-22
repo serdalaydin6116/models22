fad70378484d049ae15dd7ef710eaff6f5a7bd7e
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-08-23 01:06:27 +0300

    urls were synchronized

---
+++ august22/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import august
+
+
+
+urlpatterns = [
+    
+    path ('22', august),
+    
+]
--- main/urls.py
+++ main/urls.py
@@ -14,13 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from august.views import home
-from august22.views import august
+from django.urls import path, include
+# from august.views import home
+# from august22.views import august
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path ('', home),
-    path('august', august),
+    # path ('', home),
+    # path('august', august),
+    path ('', include('august.urls')),
+    path('', include('august22.urls')),
 ]
